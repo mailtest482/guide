5ea16b26b76dd8948dbfe673f1abbf067c534331
mailtest482 <kaist1394@gmail.com>
2024-08-07 14:30:54 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const images = [
-    'img/001.jpg', 'img/002.jpg', 'img/003.jpg', 'img/004.jpg', 'img/005.jpg',
+    'img/000.jpg', 'img/001.jpg', 'img/002.jpg', 'img/003.jpg', 'img/004.jpg', 'img/005.jpg',
     'img/006.jpg', 'img/007.jpg', 'img/008.jpg', 'img/009.jpg'
 ];
 
